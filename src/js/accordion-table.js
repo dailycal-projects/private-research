bcdf71e5e6d3c7f77b7c8bc18eda72116abf44dd
Sahil Chinoy <sahil.chinoy@gmail.com>
2017-10-09 21:28:22 -0700

    update to accordion table

---
+++ src/js/accordion-table.js
@@ -0,0 +1,53 @@
+const d3 = require('d3')
+
+var formatNumber = d3.format(".2s"),
+    format = function(d) { return "$" + formatNumber(d); };
+
+var first = 0;
+
+d3.json("../../data/accordion_table.json", function(data) {
+  Object.keys(data).forEach(function(d) {
+    var href = d.replace(/&/g, "").replace(/,./g, "").replace(/'/g, "").replace(/ /g, "_");
+
+    d3.select("#accordion")
+      .append("div")
+      //.attr("class", "panel panel-default")
+      .attr("id", `p_${href}`)
+      .attr('class', 'recipient')
+      //.append("div")
+      //.attr("class", "panel-heading")
+      //.attr("role", "tab")
+      //.attr("id", `h_${href}`)
+      .append("h4")
+      //.attr("class", "panel-title")
+      //.append("a")
+      //.attr("data-toggle", "collapse")
+      //.attr("data-parent", "#accordion")
+      .attr("href", `#${href}`)
+      //.attr("aria-expanded", "false")
+      //.attr("aria-controls", href)
+      .text(d);
+
+    var listing = d3.select(`#p_${href}`)
+                    .append("div")
+                    //.attr("class", "panel-body");
+
+    var text = "<table>";
+
+    let displayAwards = data[d].slice(0,5);
+
+    displayAwards.forEach(function(d) {
+      text += "<tr><td class='left'>" + d.sponsor + "\t</td>";
+      text += "<td class='right'>" + format(d.value) + "</td></tr>";
+    });
+
+    if (data[d].length > 5) {
+      text += `<tr><td colspan='2'>...and ${data[d].length - 5} more</td></tr>`
+    }
+
+    text += "</table>";
+
+    listing.html(text);
+    first += 1;
+  });
+});
